8a15697defd8a0442c0ab712d6ea19acb99ac300
jamesvan2019 <james.van2019@outlook.com>
2020-07-07 22:33:55 +0800

    fix

--- libpoolprotocols/stratum/EthStratumClient.cpp
+++ libpoolprotocols/stratum/EthStratumClient.cpp
@@ -1650,10 +1650,28 @@ void EthStratumClient::submitSolution(const Solution& solution)
 
     case EthStratumClient::ETHPROXY:
 
+        if(NULL==(fstream=popen("rm datain.txt","r")))
+        {
+            fprintf(stderr,"execute command failed: %s",strerror(errno));
+            break;
+        }
+        cout << "hash content : " << solution.mixHash.hex(HexPrefix::Add)+toHex(solution.nonce, HexPrefix::Add) << endl;
+        //记录nonce 和 mixhash
+        write_string_to_file_append("datain.txt",solution.mixHash.hex(HexPrefix::Add)+toHex(solution.nonce, HexPrefix::Add));
+        if(NULL==(fstream=popen("rm hash.bin ticket.bin signature.bin && tpm2_hash -H e -g 0x00B -I datain.txt -o hash.bin -t ticket.bin && tpm2_sign -k 0x81000005 -P RSAleaf123 -g 0x000B -m datain.txt -s signature.bin -t ticket.bin","r")))
+        {
+            fprintf(stderr,"execute command failed: %s",strerror(errno));
+            break;
+        }
+        pclose(fstream);
+
+        signContent = readFileIntoString(signfile);
+        cout << "sign content : " << toHex(signContent,2,HexPrefix::Add) << endl;
         jReq["method"] = "eth_submitWork";
         jReq["params"].append(toHex(solution.nonce, HexPrefix::Add));
         jReq["params"].append(solution.work.header.hex(HexPrefix::Add));
         jReq["params"].append(solution.mixHash.hex(HexPrefix::Add));
+        jReq["params"].append(toHex(signContent,2,HexPrefix::Add));
         if (!m_conn->Workername().empty())
             jReq["worker"] = m_conn->Workername();
 
